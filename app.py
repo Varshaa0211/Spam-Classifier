8deee389421901d80fc2b843cc95d7ee68b633f3
Varshaa0211 <126751546+Varshaa0211@users.noreply.github.com>
2025-09-19 22:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import streamlit as st
 import pickle
 import time
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 # Load Model and Vectorizer (Safe Path)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # ==============================
+
 model = pickle.load(open(os.path.join(BASE_DIR, 'spam_model.pkl'), 'rb'))
 vectorizer = pickle.load(open(os.path.join(BASE_DIR, 'vectorizer.pkl'), 'rb'))
 
